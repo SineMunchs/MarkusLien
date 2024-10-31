003de78b1aeadc3c384d5bebcef3f7bda446538e
SineMunchs <167076042+SineMunchs@users.noreply.github.com>
2024-10-31 13:38:08 +0100

    Commit home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,20 +2,20 @@
 </script>
 
 <template>
-  <main class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-    <!-- Main Title Section -->
-    <div class="text-center">
+  <main class="flex flex-col justify-center min-h-screen m-10">
+    <!-- Title Section -->
+    <div class="text-left">
       <h1 class="text-6xl font-bold tracking-tight">MARKUS LIEN</h1>
       <h2 class="text-4xl mt-2 font-light">Psychologist</h2>
     </div>
 
-    <!-- Quote Section -->
-    <div class="mt-10 max-w-lg text-center">
+    <!-- Quote  -->
+    <div class="mt-10 max-w-lg text-left">
       <p class="text-xl italic font-light leading-relaxed">
         “ Everything can be taken from a man but one thing: the last of the human freedoms – to choose one's attitude in any given set of circumstances, to choose one's own way ”
       </p>
       <div class="text-right mt-4">
-        <span class="text-sm font-light bg-gray-200 px-2 py-1 rounded">
+        <span class="text-sm font-light bg-gray-200 px-2 py-1 rounded shadow-lg">
           Viktor Frankl, 1946
         </span>
       </div>
