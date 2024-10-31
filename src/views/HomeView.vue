ad2bc60946e9398202041bb134928ab62fc4c6a2
SineMunchs <167076042+SineMunchs@users.noreply.github.com>
2024-10-31 12:08:22 +0100

    commit Markus lien frontpage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 style="font-size:5vw">Markus Lien</h1>
+
   </main>
 </template>
